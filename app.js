65d02444c2bb8b815ca25417cb01f18cd50b986f
Drathian <rivercoffa@gmail.com>
2024-01-16 13:51:56 +0100

    object

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let animalArray = ["apa", "orangutang", "gorilla", "get", "giraff"];
-console.log(animalArray);
+function greet(person) {
+    return `Hello ${person.name}! ${person.name} is ${person.age} years old, student: ${person.isStudent}`;
+}
+let student1 = { name: "Alexander", age: 21, isStudent: true };
+console.log(greet(student1));
--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
 let animalArray: string [] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
-console.log(animalArray);
+interface Person {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+function greet(person: Person) {
+    return `Hello ${person.name}! ${person.name} is ${person.age} years old, student: ${person.isStudent}`;
+}
+
+let student1: Person = {name: "Alexander", age: 21, isStudent: true};
+
+console.log(greet(student1));
